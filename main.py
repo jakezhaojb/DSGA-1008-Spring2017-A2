4b24891224992d2ff8e73f096d148a0a2033a1ef
Yasumasa <ym1219@nyu.edu>
2017-03-13 23:22:31 -0400

    A2 puched

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ parser.add_argument('--data', type=str, default='./data/penn',
                     help='location of the data corpus')
 parser.add_argument('--model', type=str, default='LSTM',
                     help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
-parser.add_argument('--emsize', type=int, default=200,
+parser.add_argument('--emsize', type=int, default=50,
                     help='size of word embeddings')
-parser.add_argument('--nhid', type=int, default=200,
+parser.add_argument('--nhid', type=int, default=50,
                     help='humber of hidden units per layer')
 parser.add_argument('--nlayers', type=int, default=2,
                     help='number of layers')
